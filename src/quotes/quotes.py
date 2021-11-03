1618a7f428d15c29debecf864f1fc820f3f0577f
joelzhangqc <joel.qc.ca@gmail.com>
2021-11-03 00:47:07 -0400

    1. add functions to download quotes from yahoo_fin and save to pickle files. 2. ignore pickle files.

--- src/quotes/quotes.py
+++ src/quotes/quotes.py
@@ -4,6 +4,11 @@ import pathlib
 from yahoo_fin.stock_info import get_data, tickers_sp500
 import pandas as pd
 
+from ohlclib.src.indicators.ema import ema_in_order, ema
+from ohlclib.src.patterns.engulfing import engulfing
+from ohlclib.src.patterns.hammer import hammer
+from ohlclib.src.patterns.morning_evening_star import morning_evening_star
+
 
 def get_pickle_file_name(symbol, interval):
     _script_folder = pathlib.Path().absolute()
@@ -16,7 +21,7 @@ def get_pickle_file_name(symbol, interval):
 
 def load_quotes(symbol, interval):
     df = pd.read_pickle(get_pickle_file_name(symbol, interval))
-    print(df)
+    return df
 
 
 def update_quotes_pickle(symbol, interval):
@@ -52,6 +57,26 @@ if __name__ == '__main__':
     pd.set_option('max_colwidth', 120)
     pd.options.display.width = 2080
 
-    update_quotes_spx500()
+    # update_quotes_spx500()
 
-    load_quotes('MMM', '1wk')
+    # df = load_quotes('MMM', '1d')
+
+    spx500_tickers = tickers_sp500(True)
+    num_total = spx500_tickers.shape[0]
+    i = 1
+    for row in spx500_tickers.itertuples():
+        # print(f"{i}: {row.Symbol}")
+        df = load_quotes(row.Symbol, "1d").tail(800).copy()
+        i += 1
+
+        ema_in_order(data=df, periods=[60, 120, 338])
+        ema(data=df, periods=[21])
+        engulfing(data=df)
+        hammer(data=df)
+        morning_evening_star(data=df)
+        last_row = df.iloc[-1]
+        second_last_row = df.iloc[-2]
+        if last_row.ema_in_order and last_row.ema_60_close > last_row.close > last_row.ema_120_close and last_row.close > second_last_row.close:
+            print(f"{row.Symbol}: Got it!")
+
+
